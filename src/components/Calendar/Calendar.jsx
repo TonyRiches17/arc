a993a047f7c85f5d9a4440749262a4a955095d7d
Rich <TonyRiches17@yahoo.com>
2026-07-03 10:11:53 -0400

    Add Components

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,16 @@
+import "./Calendar.css";
+
+function Calendar() {
+  // These are the packages you need to install to use the calendar and its functions
+// npm install @fullcalendar/react
+// npm install @fullcalendar/daygrid
+// npm install @fullcalendar/timegrid
+// npm install @fullcalendar/list
+// npm install @fullcalendar/interaction
+
+  <div className="calendar">
+
+  </div>
+}
+
+export default Calendar;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import "./Main.css";
+
+function Main() {
+    <div className="main">
+
+    </div>
+}
+
+export default Main;
